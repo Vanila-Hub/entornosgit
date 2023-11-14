f2f138833c578b588df1c8eacfe405c1d5d98a9c
Juan Antero <ikcth@plaiaundi.net>
2023-11-14 13:50:44 +0100

    "Primer commit"

---
+++ Ejemplo_Javadoc/src/main/MiniCalculadoraEjemplo_sinJavadoc.java
@@ -0,0 +1,32 @@
+package main;
+
+
+
+/**
+ * Esta clase representa una calculadora simple que realiza operaciones de suma,
+ * resta, multiplicación y división.
+ * 
+ * @author juanAntero
+ * @since 2023-10-17 
+ * @version 2.0
+ * 
+ */
+
+public class MiniCalculadoraEjemplo_sinJavadoc {
+   
+    /**
+     * 
+     */
+    public MiniCalculadoraEjemplo_sinJavadoc (){
+    }//Cierre del constructor
+   
+    
+    public double dimeValorAbsoluto (double m) {
+        return Math.abs (m);
+    }//Cierre del método
+   
+    
+    public double dimeRaizCuadrada (double n) {
+        return Math.sqrt (n);
+    }//Cierre del método
+}//Cierre de la clase
